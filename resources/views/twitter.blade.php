3dd0db2788d8782658deff21de53b24964635994
erekoba <erecasio0302@gmail.com>
2019-11-17 10:02:32 +0900

    見た目整えた

--- resources/views/twitter.blade.php
+++ resources/views/twitter.blade.php
@@ -1,28 +1,124 @@
  @extends('layouts.header')
  @section('content')
+ <div class="main">
+     <div class="content">
+         <div class=chara_top>
+             <div class=chara>
+                 <img id=chara_img src="/storage/chara_picture/ACE.png">
 
- <div class="container">
-     <!-- コントローラーで取得した$resultをforeachで回す -->
-     @foreach ($result as $tweet)
-     <div class="card mb-2">
-         <div class="card-body">
-             <div class="media">
-                 <img src="https://placehold.jp/70x70.png" class="rounded-circle mr-4">
-                 <div class="media-body">
-                     <h5 class="d-inline mr-3"><strong></strong></h5>
-                     <h6 class="d-inline text-secondary"></h6>
-                     <p class="mt-3 mb-0">{{$tweet->text}}</p>
+                 <div class="chara_name">エース(ONE PIECE)</div>
+             </div>
+             <div class=guild_status>
+                 <div style="text-align:start;">ギルドランク
+                 </div>
+                 <img id=guild_icon src="storage/icon/盾.svg" style="height:62px">
+                 <div style="color:#FF8500">オレンジ</div>
+                 <div class=chara_population>
+                     <div class=pop style="color:#969696;">総ファン数</div>
+                     <div class=pop style="font-size:16px;font-weight: 700;">3,767</div>
+                     <div class=pop style="color:#969696;">人</div>
                  </div>
              </div>
          </div>
-         <div class="card-footer bg-white border-top-0">
-             <div class="d-flex flex-row justify-content-end">
-                 <div class="mr-5"><i class="far fa-comment text-secondary"></i></div>
-                 <div class="mr-5"><i class="fas fa-retweet text-secondary"></i></div>
-                 <div class="mr-5"><i class="far fa-heart text-secondary"></i></div>
+         <div class="join_btn">
+             <div>ギルドに参加する</div>
+         </div>
+         <div class="middle_bar">
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_1 style="padding:6px 0 3px 0 ;">ギルド</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_2 style="padding:6px 0 3px 0 ;">タイムライン</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_3 style="padding:6px 0 3px 0 ;">画像倉庫</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_4 style="padding:6px 0 3px 0 ;">募集</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_5 style="padding:6px 0 3px 0 ;">クエスト</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_6 style="padding:6px 0 3px 0 ;">好きなセリフ</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=forborder>
+                     <div class=middle_bar_7 style="padding:6px 0 3px 0 ;">告知</div>
+                 </div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_1 style="padding:6px 0 3px 0 ;">ギルド</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <a href="{{url('timeline')}}" style="text-decoration: none;color:#333333">
+                     <div class=middle_bar_2 style="padding:6px 0 3px 0 ;">
+                         タイムライン
+                     </div>
+                 </a>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_3 style="padding:6px 0 3px 0 ;">画像倉庫</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_4 style="padding:6px 0 3px 0 ;">募集</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_5 style="padding:6px 0 3px 0 ;">クエスト</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_6 style="padding:6px 0 3px 0 ;">好きなセリフ</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_7 style="padding:6px 0 3px 0 ;">告知</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_1 style="padding:6px 0 3px 0 ;">ギルド</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_2 style="padding:6px 0 3px 0 ;">タイムライン</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_3 style="padding:6px 0 3px 0 ;">画像倉庫</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_4 style="padding:6px 0 3px 0 ;">募集</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_5 style="padding:6px 0 3px 0 ;">クエスト</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_6 style="padding:6px 0 3px 0 ;">好きなセリフ</div>
+             </div>
+             <div class=middle_bar_outline>
+                 <div class=middle_bar_7 style="padding:6px 0 3px 0 ;">告知</div>
              </div>
          </div>
+         <hr style="padding:0px;margin:0px;background-color: #EFEFEF;">
+         <div class="container">
+             <!-- コントローラーで取得した$resultをforeachで回す -->
+             @foreach ($result as $tweet)
+             <div class="card mb-2">
+                 <div class="card-body">
+                     <div class="media">
+                         <img src="https://placehold.jp/70x70.png" class="rounded-circle mr-4">
+                         <div class="media-body">
+                             <h5 class="d-inline mr-3"><strong></strong></h5>
+                             <h6 class="d-inline text-secondary"></h6>
+                             <p class="mt-3 mb-0">{{$tweet->text}}</p>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="card-footer bg-white border-top-0">
+                     <div class="d-flex flex-row justify-content-end">
+                         <div class="mr-5"><i class="far fa-comment text-secondary"></i></div>
+                         <div class="mr-5"><i class="fas fa-retweet text-secondary"></i></div>
+                         <div class="mr-5"><i class="far fa-heart text-secondary"></i></div>
+                     </div>
+                 </div>
+             </div>
+             @endforeach
+         </div>
      </div>
-     @endforeach
  </div>
  @endsection
