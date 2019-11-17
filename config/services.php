52fc6e97fff8dc1634cab4d3a772385bc1680022
erekoba <erecasio0302@gmail.com>
2019-11-17 15:49:49 +0900

    no message

--- config/services.php
+++ config/services.php
@@ -31,7 +31,7 @@ return [
     ],
 
     'twitter' => [
-        'client_id' => env('TWITTER_CLIENT_ID'),
+        'client_id' => env('TWITTER_CLIENT_KEY'),
         'client_secret' => env('TWITTER_CLIENT_SECRET'),
         'redirect' => env('TWITTER_CLIENT_CALLBACK')
     ],
